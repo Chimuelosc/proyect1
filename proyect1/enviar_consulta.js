fb083fc623d0722496e719397df6d667e2cee8da
Chimuelosc <nadyabpla@gmail.com>
2023-05-29 22:32:37 -0300

    Se agrega el proyecto

---
+++ proyect1/enviar_consulta.js
@@ -0,0 +1,6 @@
+$nombre_form = $_POST['nombre'];
+$apellido_form = $_POST['apellido'];
+$correo_form = $_POST['correo'];
+$consulta_form = $_POST['consulta'];
+
+header("location: formulario.html")
---
+++ proyect1/unidad7.js
@@ -0,0 +1,7 @@
+var nombre = prompt('HOLA!¿Cómo te llamas?')
+var estilo = prompt('¿Qué estilo prefieres entre clasico/deportivo/fashion?')
+var colorCombinado = prompt('¿Qué color prefieres a la hora de combinar?')
+var calzado = prompt('¿Prefieres sandalias, zapatillas ó tacones?')
+var prendaInferior = prompt('¿Y entre la bermuda, el pantalon ó la falda cúal elegirías?')
+var fiesta = prompt('¿Para una fiesta cual te gusta más, de vestido, chaqueta ó una combinación mixta?')
+alert('Acompañanos '+nombre+' ¡Esta pagina te mostrara una parte de nuestros productos, no dejes de seguirnos!')
